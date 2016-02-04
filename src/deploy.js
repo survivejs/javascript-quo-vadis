fe25f144477b65c42fa5d349320a27bbd867fc2a
Juho Vepsalainen <bebraw@gmail.com>
2016-02-04 18:18:14 +0200

    Fix deployment script

--- src/deploy.js
+++ src/deploy.js
@@ -1,11 +1,7 @@
 /* eslint no-console:0 */
 const ghpages = require("gh-pages");
 
-main();
-
-const main = () => {
-  ghpages.publish("./out", {
-    silent: false,
-    logger: console.log.bind(console)
-  }, console.error.bind(console));
-};
+ghpages.publish("./out", {
+  silent: false,
+  logger: console.log.bind(console)
+}, console.error.bind(console));
